290b81adcf29a3b0b423c0ce8865932ca11ac53f
silveiraJeferson <jefer.ls@hotmail.com>
2017-08-06 11:47:19 -0300

    xxx

--- leite/resources/views/hackathon/folders/gestao_animais/index.blade.php
+++ leite/resources/views/hackathon/folders/gestao_animais/index.blade.php
@@ -5,7 +5,7 @@
     <input  class="form-control center-block" id="busca-vaca" placeholder="Codigo da Vaca">
   </div>
 <div class="inicio-animais ">
-    <a class="btn btn-default azul-milkon btn-inicio" href="{{url('/ver/19')}}">Buscar</a><br/><br/>
+    <a class="btn btn-default azul-milkon btn-inicio" href="{{url('/ver/1')}}">Buscar</a><br/><br/>
 
     <a class="btn btn-default azul-milkon btn-inicio" href="{{url('/adicionar')}}">Cadastrar</a>
 
